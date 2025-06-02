f6ed61358746f12c78ac4dcb6ab44d16129b7046
omprakash <omprakashreddysaggam9541@gmail.com>
2025-06-02 19:54:29 +0200

    first commit

---
+++ src/main/java/com/membership/chat_bot/ChatBotV1Application.java
@@ -0,0 +1,13 @@
+package com.membership.chat_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotV1Application.class, args);
+    }
+
+}
